9188750d77cd139c822e0b737a945dde6f34f2ad
Raul Paiva <raulejphp2009@gmail.com>
2026-03-26 19:44:46 +0000

    Correção de Erro de Merge no Github

---
+++ src/modules/auth/auth-routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { AuthController } from './auth-controller';
+
+const router = Router();
+const authController = new AuthController();
+
+router.get('/login', (req, res) => {
+    res.sendFile('login.html', { root: './src/modules/auth/views' });
+});
+
+router.get('/register', (req, res) => {
+    res.sendFile('register.html', { root: './src/modules/auth/views' });
+});
+
+router.post('/register', (req, res) => authController.register(req, res));  
+router.post('/login', (req, res) => authController.login(req, res));
+router.post('/logout', (req, res) => authController.logout(req, res));
+
+export { router as AuthRoutes };
