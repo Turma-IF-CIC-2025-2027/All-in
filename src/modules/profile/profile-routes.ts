313f8e52f90da1f48a613a635524bd04ffec9254
LHoly08 <santosdiasluis08@gmail.com>
2026-05-10 10:15:25 +0000

    Restructuring of the Project
    Changed the language of the project from Typescript to PHP
    Added also some example code

--- src/modules/profile/profile-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { ProfileController } from './profile-controller';
-
-const router = Router();
-const profileController = new ProfileController();
-
-//router.get('/me', ProfileMiddleware, (req, res) => profileController.getProfile(req, res));
-
-export { router as ProfileRoutes };
