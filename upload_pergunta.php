51d4c28bdd01a4585eb7d28c9a47e4f62e4b3ed7
FR4NC1SC0 <franciscoxavier2508@gmail.com>
2026-05-13 09:55:24 +0100

    Update upload_pergunta.php

--- upload_pergunta.php
+++ upload_pergunta.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once "db_connect.php";
+require_once '../php/db_connect.php';
 
 if (
     !isset($_SESSION['id_user']) ||
@@ -23,7 +23,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $resposta_correta = $_POST['resp_correta'];
     $dificuldade = intval($_POST['perg_dificuldade']);
 
-    $imagem_path = null;
+    if ((isset($_FILES['perg_imagem'])) && ($_FILES['perg_imagem']['error'] === 0)) {
+        $f = $_FILES['perg_imagem'];
+
+        if ($f['size'] > 2097152) die("Ficheiro muito grande! Máximo 2MB.");
+
+        $extensao = strtolower(pathinfo($f['name'], PATHINFO_EXTENSION));
+        if ($extensao != "jpg" && $extensao != "png") die("Formato inválido. Use JPG ou PNG.");
+
+        $novo_nome = time() . "_" . $f['name'];
+        $destino = "../uploads/" . $novo_nome;
+
+        if (move_uploaded_file($f['tmp_name'], $destino)) $imagem_path = $destino; 
+        else die("Erro ao mover o ficheiro para ../uploads/ .");
+    }
+    else $imagem_path = null;
 
     $sql = "
         INSERT INTO tb_questions
@@ -68,5 +82,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     mysqli_stmt_close($stmt);
 }
-?>
 ?>
