26625ccc2ea2ce1ae894a8981a344e520046dc6f
Oliveira-27 <goncaloliveira06022099@gmail.com>
2026-05-17 12:26:58 +0100

    Comments and links
    I´ve updated the files, now both the footer.php and rankings.php have the right link to the css file. The css file is now comented so it can be understood easier.

--- rankings.php
+++ rankings.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ranking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/main.css">
 </head>
 <body>
 <?php
@@ -21,7 +22,6 @@
 
     $resultado = mysqli_query($conn, $query);
 ?>
-
 
 
 <div class="rk-wrap">
