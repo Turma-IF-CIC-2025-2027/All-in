8eb86e4b5302b5391b39189ea67965dc08a6f9fc
FR4NC1SC0 <franciscoxavier2508@gmail.com>
2026-05-20 08:51:59 +0100

    Refactor upload_pergunta.php for improved structure

--- upload_pergunta.php
+++ upload_pergunta.php
@@ -1,16 +1,11 @@
 <?php
 session_start();
 
-require_once '../php/db_connect.php';
+require_once 'db_connect.php';
+require_once 'sessao.php';
+require_once 'upload_image.php';
 
-if (
-    !isset($_SESSION['id_user']) ||
-    ($_SESSION['tipo_user'] != 'prof' &&
-     $_SESSION['tipo_user'] != 'admin')
-) {
-    header("Location: ../public/login.php");
-    exit();
-}
+verificar_tipo(professor);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id_disciplina = intval($_POST['id_disciplina']);
@@ -23,21 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $resposta_correta = $_POST['resp_correta'];
     $dificuldade = intval($_POST['perg_dificuldade']);
 
-    if ((isset($_FILES['perg_imagem'])) && ($_FILES['perg_imagem']['error'] === 0)) {
-        $f = $_FILES['perg_imagem'];
-
-        if ($f['size'] > 2097152) die("Ficheiro muito grande! Máximo 2MB.");
-
-        $extensao = strtolower(pathinfo($f['name'], PATHINFO_EXTENSION));
-        if ($extensao != "jpg" && $extensao != "png") die("Formato inválido. Use JPG ou PNG.");
-
-        $novo_nome = time() . "_" . $f['name'];
-        $destino = "../uploads/" . $novo_nome;
-
-        if (move_uploaded_file($f['tmp_name'], $destino)) $imagem_path = $destino; 
-        else die("Erro ao mover o ficheiro para ../uploads/ .");
-    }
-    else $imagem_path = null;
+    $image_path = processarImagemUpload()
 
     $sql = "
         INSERT INTO tb_questions
@@ -74,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     );
 
     if (mysqli_stmt_execute($stmt)) {
-        header("Location: ../public/lista_perguntas.php?sucesso=1");
+        header("Location: lista_perguntas.php?sucesso=1");
         exit();
     } else {
         echo "Erro ao guardar pergunta.";
